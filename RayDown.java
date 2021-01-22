f33ce3782bcd04c9f2255b603c19c9152319eca6
600020344 <77069279+600020344@users.noreply.github.com>
2021-01-22 09:42:36 -0800

    Update RayDown.java

--- RayDown.java
+++ RayDown.java
@@ -9,6 +9,16 @@ public class RayDown
  //are in decreasing order [31,12,6,2,1]
  public static boolean go(int[] numArray)
  {
+   int first, second;
+   for (int i = 1; i<numArray.length; i++)
+   {
+     first = numArray[i-1];
+     second = numArray[i];
+     if (first<=second)
+     {
+       return false;
+     }
+   }
   return true;
  } 
 }
